98bfb1c0445d63a37800a9f7eeeaae21a371ac51
Ivan Goncalves <igoncalves@thecode.co>
2020-09-16 18:48:38 +1000

    Code Clean up / Add eslint / Fix deprecation problem with MongoDB / Delete package-lock.json

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User'); //has to come before services/passport, position matter here.
 require('./services/passport'); 
-mongoose.connect(keys.mongoURI);
+
+// DB Connect
+mongoose.connect(keys.mongoURI, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Init Express
 const app = express();
 
-app.use(bodyParser.json());
-
+// Middlewares
+app.use(express.json());
 app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+	cookieSession({
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
 );
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -28,20 +32,14 @@ require('./routes/googleAuthRoutes')(app);
 require('./routes/facebookAuthRoutes')(app);
 //require('./routes/appleAuthRoutes')(app);
 
-//test 
-/*
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});*/
-
 if (process.env.NODE_ENV === 'production') {
-  //express will serve up production assets like our main.js file, or main.css file
-  app.use(express.static('client/build'));
-  //express will serve up the index.html file if it doesnt recognize the route
-  const path = require('path');
-  app.use('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+	//express will serve up production assets like our main.js file, or main.css file
+	app.use(express.static('client/build'));
+	//express will serve up the index.html file if it doesnt recognize the route
+	const path = require('path');
+	app.use('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
