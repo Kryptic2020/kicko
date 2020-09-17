0e5a2a524434b3ec64df251a0cdc409c70d672b5
Ivan Goncalves <igoncalves@thecode.co>
2020-09-17 17:38:39 +1000

    Fixed folder structure / Added types for react-redux and react-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
-//import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/App';
-import reducers from './reducers';
+import Login from './pages/Login';
+import reducers from './store/reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <Login />
   </Provider>,
   document.querySelector('#app')
 );
