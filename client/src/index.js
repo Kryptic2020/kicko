a3bb155d6bebffc589718dc2ea0e0a2ce2608404
Ivan Goncalves <igoncalves@thecode.co>
2020-09-23 13:21:00 +1000

    Front end stuff

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,17 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import Login from './pages/Login';
 import reducers from './store/reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
-
 ReactDom.render(
   <Provider store={store}>
-    <Login />
+      <CssBaseline />
+      <Login />
   </Provider>,
   document.querySelector('#app')
 );
--- client/src/pages/Login/styles.js
+++ client/src/pages/Login/styles.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
+export const SocialButton = styled.button`
+    color: #FFFFFF;
+    padding: 13px 22px;
+    box-shadow: none;
+    margin-bottom: 10px;
+    text-transform: initial;
+    justify-content: end;
+    background-color: #4B9F5C;
+    border: 0;
+    cursor: pointer;
+    margin: 0;
+    display: inline-flex;
+    outline: 0;
+    width: 100%;
 `;
